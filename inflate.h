70025fce90f28091fbc89f90de52ec79e45c8bb5
Steven Noonan <steven@uplinklabs.net>
2021-07-20 12:35:37 -0700

    introduce build system for game projects
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- inflate.h
+++ inflate.h
@@ -3,6 +3,9 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
+#ifndef _INCLUDED_ZLIB_INFLATE_H
+#define _INCLUDED_ZLIB_INFLATE_H
+
 /* WARNING: this file should *not* be used by applications. It is
    part of the implementation of the compression library and is
    subject to change. Applications should only use zlib.h.
@@ -124,3 +127,5 @@ struct inflate_state {
     int back;                   /* bits back of last unprocessed length/lit */
     unsigned was;               /* initial length of match */
 };
+
+#endif
