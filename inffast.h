d1e9144a7375d3451d9d3a86bf058ef71e557567
Steven Noonan <steven@uplinklabs.net>
2023-12-28 13:33:46 -0800

    headers: add header include guards to enable unity builds
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- inffast.h
+++ inffast.h
@@ -3,9 +3,14 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
+#ifndef _INCLUDED_ZLIB_INFFAST_H
+#define _INCLUDED_ZLIB_INFFAST_H
+
 /* WARNING: this file should *not* be used by applications. It is
    part of the implementation of the compression library and is
    subject to change. Applications should only use zlib.h.
  */
 
 void ZLIB_INTERNAL inflate_fast(z_streamp strm, unsigned start);
+
+#endif
